0f289581e432fcc50c8ec43dbd1b7279eaa03f72
Sam Hocevar <sam@hocevar.net>
2017-08-19 22:12:14 +0200

    Improve Gaussian elimination performance by removing recursion.
    Storing the exact representation of e_k (and therefore f_k) will
    also make subsequent computations of the polynomial approximation
    a lot easier.

--- src/lolremez2d.cpp
+++ src/lolremez2d.cpp
@@ -22,47 +22,93 @@ using namespace lol;
 
 real f(real const &x, real const &y)
 {
-    static array<real,real,real> cache;
     static real const one = real::R_1();
-
-    for (auto const &v : cache)
-        if (x == v.m1 && y == v.m2)
-            return v.m3;
 
     //return exp(sin(x+x+x)+cos(y-one/4));
     real f = (x + one) / 2;
     real d = (y + one) / 2;
-    real ret = sin((one - f) * acos(d)) / sqrt(one - d * d);
-
-    cache.push(x, y, ret);
-    return ret;
+    return sin((one - f) * acos(d)) / sqrt(one - d * d);
 }
 
 struct solver
 {
-    solver(int grid)
-      : m_grid(grid)
+    solver(int grid_size, int iters)
+      : m_grid_size(grid_size),
+        m_iters(iters),
+        m_ek(ivec2(iters))
     {
-        for (int i = 0; i <= grid; ++i)
-            m_coeff.push(cheb(i, grid));
+        for (int i = 0; i <= grid_size; ++i)
+            m_coeff.push(cheb(i, grid_size));
     }
 
     void step()
     {
-        rvec2 best(0);
-        real max_abs = real::R_0();
+        /* Find a new good pivot */
+        rvec2 best_pivot(0,0);
+        real best_val(0);
 
         for (auto const &y : m_coeff)
         for (auto const &x : m_coeff)
         {
-            real ret = abs(e_n(m_pivots.count(), x, y));
-            if (ret > max_abs)
+            real res = eval_ek(x, y);
+            if (abs(res) >= abs(best_val))
             {
-                best = rvec2(x,y);
-                max_abs = ret;
+                best_pivot = rvec2(x,y);
+                best_val = res;
             }
         }
-        m_pivots.push(rvec3(best, real::R_1() / e_n(m_pivots.count(), best.x, best.y)));
+
+        /* Compute d_k = 1/e_{k-1}(x,y) */
+        real dk = real::R_1() / best_val;
+
+        /* Compute e_{k-1}(x_k,y) as an array of f(x_i,y) components
+         * and e_{k-1}(x,y_k) as an array of f(x,y_i) components */
+        array<real> ek_x, ek_y;
+        ek_x.resize(m_pivots.count() + 1);
+        ek_y.resize(m_pivots.count() + 1);
+        for (int i = 0; i < m_pivots.count(); ++i)
+            for (int j = 0; j < m_pivots.count(); ++j)
+                if (m_ek[i][j])
+                {
+                    ek_x[j] += m_ek[i][j] * eval_f(m_pivots[i].x, best_pivot.y);
+                    ek_y[i] += m_ek[i][j] * eval_f(best_pivot.x, m_pivots[j].y);
+                }
+        ek_x[m_pivots.count()] = 1; /* implicit f */
+        ek_y[m_pivots.count()] = 1; /* implicit f */
+
+        /* Compute new fk */
+        for (int i = 0; i < m_pivots.count() + 1; ++i)
+            for (int j = 0; j < m_pivots.count() + 1; ++j)
+                m_ek[i][j] -= ek_y[i] * ek_x[j] * dk;
+
+        /* Register new pivot */
+        m_pivots.push(best_pivot);
+    }
+
+    real eval_f(real const &x, real const &y)
+    {
+        static array<real,real,real> cache;
+
+        for (auto const &v : cache)
+            if (x == v.m1 && y == v.m2)
+                return v.m3;
+
+        cache.push(x, y, f(x, y));
+        return cache.last().m3;
+    }
+
+    real eval_ek(real const &x, real const &y)
+    {
+        /* Evaluate e_k at x,y: first, the implicit f part */
+        real ret = eval_f(x, y);
+
+        /* Then, the f(x_i,y)*f(x,y_j) parts */
+        for (int i = 0; i < m_pivots.count(); ++i)
+            for (int j = 0; j < m_pivots.count(); ++j)
+                if (m_ek[i][j])
+                    ret += m_ek[i][j] * eval_f(m_pivots[i].x, y) * eval_f(x, m_pivots[j].y);
+
+        return ret;
     }
 
     void dump_gnuplot()
@@ -84,39 +130,38 @@ struct solver
     }
 
 private:
-    real d_n(int i, real const &x, real const &y)
-    {
-        return e_n(i, x, m_pivots[i].y) * e_n(i, m_pivots[i].x, y) * m_pivots[i].z;
-    }
-
-    real e_n(int i, real const &x, real const &y)
-    {
-        return i == 0 ? f(x, y) : e_n(i - 1, x, y) - d_n(i - 1, x, y);
-    }
-
-    real f_n(int i, real const &x, real const &y)
-    {
-        return i == 0 ? real::R_0() : f_n(i - 1, x, y) + d_n(i - 1, x, y);
-    }
-
     real cheb(int i, int n)
     {
         return -cos(real::R_PI() * i / n) * real("0.999999999999999");
     }
 
-    int m_grid;
+    int m_grid_size;
+    int m_iters;
+
+    /*
+     * Our “meta-function” structure. It has a matrix of coefficients that
+     * contains the contribution of f(x_i,y)f(x,y_j) to this function. Since
+     * this function is the error function, it has an implicit f(x,y) added
+     * to it because of the algorithm’s setup.
+     *
+     * Using this kind of storage, we can add meta-functions together, and
+     * evaluate them at a given point or along a given x or y line.
+     */
+    array2d<real> m_ek;
+
     array<real> m_coeff;
-    array<rvec3> m_pivots;
+    array<rvec2> m_pivots;
 };
 
 int main(int argc, char **argv)
 {
     UNUSED(argc, argv);
 
+    int const grid_size = 33;
     int const iters = 6;
 
     /* Create solver and iterate */
-    solver s(33);
+    solver s(grid_size, iters);
     for (int n = 0; n < iters; ++n)
         s.step();
 
